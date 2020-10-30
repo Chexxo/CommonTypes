c7c05bbbfb3c1241986675b3a973e0222b276810
Vec7or <carlo@kirchmeier.co>
2020-10-30 11:31:20 +0100

    Add ServerError

---
+++ errors/ServerError.ts
@@ -0,0 +1,23 @@
+import CodedError from "./CodedError";
+
+/**
+ * Defines a not handled error inside the server component.
+ */
+export default class ServerError extends CodedError {
+  readonly name: string;
+  readonly message: string;
+  readonly stack?: string;
+  readonly code = 500;
+  readonly publicMessage = "An internal server error occured.";
+
+  /**
+   *
+   * @param error The error which lead to this exception.
+   */
+  constructor(error: Error) {
+    super();
+    this.name = error.name;
+    this.message = error.message;
+    this.stack = error.stack;
+  }
+}
