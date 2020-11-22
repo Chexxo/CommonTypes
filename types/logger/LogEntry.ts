27b1e8c1ca0376a3f47dec4c56d50e0486b9efdc
Vec7or <carlo@kirchmeier.co>
2020-11-22 18:44:46 +0100

    Add documentation

--- types/logger/LogEntry.ts
+++ types/logger/LogEntry.ts
@@ -1,6 +1,10 @@
 import { LogLevel } from "../../logger/Logger";
 import { CodedError } from "../errors/CodedError";
 
+/**
+ * Represents a log entry. Which was either read
+ * from the log or will be written into the log.
+ */
 export class LogEntry {
   constructor(
     readonly logLevel: LogLevel,
