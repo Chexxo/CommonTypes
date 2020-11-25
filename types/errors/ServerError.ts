7e813c76c91ca216f085c8c4a111a95436811c0f
Vec7or <carlo@kirchmeier.co>
2020-11-25 16:17:12 +0100

    Adjust errors

--- types/errors/ServerError.ts
+++ types/errors/ServerError.ts
@@ -4,21 +4,24 @@ import { CodedError } from "./CodedError";
  * Defines a unhandled error of the Chexxo server API.
  */
 export class ServerError extends CodedError {
-  readonly name: string;
-  readonly message: string;
+  readonly name: string = "Server error";
+  readonly message: string = "An internal server error occured.";
   readonly trace?: string;
 
   readonly code = 500;
   readonly publicMessage = "An internal server error occured.";
 
   /**
+   * @param uuid The uuid that identifies this particular error.
    * @param error The error which lead to this exception.
    */
-  constructor(readonly uuid: string, error: Error) {
+  constructor(readonly uuid: string, error?: Error) {
     super();
-    super.stack = error.stack;
-    this.name = error.name;
-    this.message = error.message;
-    this.trace = error.stack;
+    if (error) {
+      super.stack = error.stack;
+      this.name = error.name;
+      this.message = error.message;
+      this.trace = error.stack;
+    }
   }
 }
